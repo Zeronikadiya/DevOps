8fa2189bf02b66744132d5f4b2b9c97634fe2e45
Divya <zeronikadiya@gmail.com>
2026-05-13 12:25:36 +0000

    code update

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -1,7 +1,12 @@
 package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello from CI/CD Pipeline!");
+        SpringApplication.run(App.class, args);
     }
 }
-EOF
